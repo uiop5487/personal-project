aacfd384d22ca5bdd8b50a059f79646c88a9a2f0
uiop5487 <uiop5487@gmail.com>
2022-07-17 19:43:20 +0900

    Update FadeIn

--- mini-project/src/component/units/rending/rending.presenter.tsx
+++ mini-project/src/component/units/rending/rending.presenter.tsx
@@ -1,6 +1,8 @@
 import { useCallback, useEffect, useMemo, useState } from "react";
 import Slider from "react-slick";
 import * as S from "./rending.styles";
+// @ts-ignore
+import Fade from "react-reveal/Fade";
 
 const imageArr = [
   "./img/travel1.jpg",
@@ -11,28 +13,54 @@ const imageArr = [
 
 export default function RendingPagePresenter() {
   const [image, setImage] = useState("");
+  const [isActive, setIsActive] = useState([true, false, false, false]);
 
-  const aaa = useCallback(() => {
-    let count = 0;
-    console.log(count);
-    setInterval(() => {
-      setImage(imageArr[count]);
-      if (count === 3) {
-        count = 0;
-      }
-      count++;
-    }, 2500);
-  }, []);
+  const aaa = () => {
+    if (isActive[0]) {
+      const arr = [false, true, false, false];
+      setIsActive(arr);
+    }
+    if (isActive[1]) {
+      const arr = [false, false, true, false];
+      setIsActive(arr);
+    }
+    if (isActive[2]) {
+      const arr = [false, false, false, true];
+      setIsActive(arr);
+    }
+    if (isActive[3]) {
+      const arr = [true, false, false, false];
+      setIsActive(arr);
+    }
+  };
 
   useEffect(() => {
-    aaa();
-  }, []);
-
+    setTimeout(() => {
+      aaa();
+    }, 4500);
+  }, [isActive]);
   return (
-    <div>
-      <S.Wrapper>
-        <S.SlickBox1 image={image}></S.SlickBox1>
-      </S.Wrapper>
-    </div>
+    <S.Wrapper>
+      {isActive[0] && (
+        <Fade>
+          <S.ImgBox1 image={imageArr[0]}></S.ImgBox1>
+        </Fade>
+      )}
+      {isActive[1] && (
+        <Fade>
+          <S.ImgBox2 image={imageArr[1]}></S.ImgBox2>
+        </Fade>
+      )}
+      {isActive[2] && (
+        <Fade>
+          <S.ImgBox3 image={imageArr[2]}></S.ImgBox3>
+        </Fade>
+      )}
+      {isActive[3] && (
+        <Fade>
+          <S.ImgBox4 image={imageArr[3]}></S.ImgBox4>
+        </Fade>
+      )}
+    </S.Wrapper>
   );
 }
--- mini-project/src/component/units/rending/rending.styles.ts
+++ mini-project/src/component/units/rending/rending.styles.ts
@@ -19,37 +19,38 @@ interface ImageProps {
   image: any;
 }
 
-export const SlickBox1 = styled.div`
+interface IsActiveProps {
+  isActive: boolean;
+}
+
+export const ImgBox1 = styled.div`
   width: 100vw;
   height: 100vh;
   background-image: linear-gradient(rgba(0, 0, 0, 0.3), rgba(0, 0, 0, 0.3)),
     url(${(props: ImageProps) => props.image});
   background-size: cover;
 `;
-export const SlickBox2 = styled.div`
+
+export const ImgBox2 = styled.div`
   width: 100vw;
   height: 100vh;
-  background-image: linear-gradient(rgba(0, 0, 0, 0.4), rgba(0, 0, 0, 0.4)),
-    url("./img/travel2.jpg");
-  background-size: cover;
-`;
-export const SlickBox3 = styled.div`
-  width: 100vw;
-  height: 100vh;
-  background-image: linear-gradient(rgba(0, 0, 0, 0.3), rgba(0, 0, 0, 0.4)),
-    url("./img/travel3.jpg");
-  background-size: cover;
-`;
-export const SlickBox4 = styled.div`
-  width: 100vw;
-  height: 100vh;
-  background-image: linear-gradient(rgba(0, 0, 0, 0.2), rgba(0, 0, 0, 0.4)),
-    url("./img/travel4.jpg");
+  background-image: linear-gradient(rgba(0, 0, 0, 0.3), rgba(0, 0, 0, 0.3)),
+    url(${(props: ImageProps) => props.image});
   background-size: cover;
 `;
 
-export const Box = styled.div`
+export const ImgBox3 = styled.div`
   width: 100vw;
-  height: 100vw;
-  background-color: white;
+  height: 100vh;
+  background-image: linear-gradient(rgba(0, 0, 0, 0.3), rgba(0, 0, 0, 0.3)),
+    url(${(props: ImageProps) => props.image});
+  background-size: cover;
+`;
+
+export const ImgBox4 = styled.div`
+  width: 100vw;
+  height: 100vh;
+  background-image: linear-gradient(rgba(0, 0, 0, 0.3), rgba(0, 0, 0, 0.3)),
+    url(${(props: ImageProps) => props.image});
+  background-size: cover;
 `;
